dc2cc2b977ad42421ead4ff67b6af99449b72849
Peter H. Boling <peter.boling@gmail.com>
2026-07-31 21:52:36 -0600

    🎨 Template bootstrap by kettle-jem v7.1.0

--- spec/rubocop/lts/version_spec.rb
+++ spec/rubocop/lts/version_spec.rb
@@ -1,3 +1,14 @@
+require "anonymous_loader"
 RSpec.describe Rubocop::Lts::Version do
   it_behaves_like "a Version module", described_class
+
+  it "executes the version file for coverage without redefining constants" do
+    paths = [
+      File.expand_path("../../../lib/rubocop/lts/version.rb", __dir__),
+      File.expand_path("../../../lib/rubocop/lts/version_gem.rb", __dir__)
+    ].select { |path| File.file?(path) }
+    anonymous_namespace = AnonymousLoader.load(files: paths)
+
+    expect(anonymous_namespace::Rubocop::Lts::Version::VERSION).to eq(described_class::VERSION)
+  end
 end
