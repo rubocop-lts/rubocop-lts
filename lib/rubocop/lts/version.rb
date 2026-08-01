0d93db10df51cfde12dc4f87947ad6fa5c8d9634
Peter H. Boling <peter.boling@gmail.com>
2026-07-31 21:54:21 -0600

    🎨 Template bootstrap by kettle-jem v7.1.0

--- lib/rubocop/lts/version.rb
+++ lib/rubocop/lts/version.rb
@@ -2,9 +2,12 @@
 
 module Rubocop
   module Lts
+    # Version namespace for this gem.
     module Version
+      # Current gem version.
       VERSION = "22.3.2"
     end
+    # Current gem version exposed at the traditional constant location.
     VERSION = Version::VERSION # Traditional Constant Location
   end
 end
